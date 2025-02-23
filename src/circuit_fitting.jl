69f957ed31a56234b2f277d97661fd11443970e1
bdamerau <35343746+bdamerau@users.noreply.github.com>
2025-02-23 10:44:56 -0500

    Update circuit_fitting.jl

--- src/circuit_fitting.jl
+++ src/circuit_fitting.jl
@@ -1,34 +1,3 @@
-#I wanna be able to fit without needing to provide any bounds or parameters
-function circuit_fit(circuit, ω_data, Z_data)
-
-    #getting initial parameters and the parameter shape
-    tuples = findall(x->typeof(x)!=Float64,get_params(circuit))
-    tuples += collect(0:length(tuples)-1)
-    unflatten = x->unflatten_parameters(x,tuples)
-
-    p = get_params(circuit)
-    p0 = collect(Iterators.flatten(p))
-
-    function fitting_function(ω,p)
-        p_shaped = unflatten(p)
-        Z = (set_params(circuit,p_shaped)~ω).Z
-        return vcat(real(Z),imag(Z))
-    end
-    fit = curve_fit(fitting_function, ω_data, vcat(real(Z_data),imag(Z_data)), p0;
-        lower = zeros(length(p0)))
-    p_fit = unflatten(round.(fit.param,sigdigits = 5))
-    circuit_fit = set_params(circuit,p_fit)
-    println("Parameters")
-    println("__________")
-    print(circuit_fit.elements)
-    circuit_fit = circuit_fit ~ ω_data
-    Z_fit = circuit_fit.Z
-    plt = scatter(Z_data,label = "data")
-    scatter!(plt,Z_fit,markersize=3,label = "fit")
-    plot!(plt,aspect_ratio=:equal,yflip=true,ylabel = "-Im Z / Ω", xlabel = "Re Z / Ω",legend = :topleft)
-    display(plt)
-    return circuit_fit
-end
 
 #shaping the parameters to map onto circuit elements 
 function unflatten_parameters(pflat,tuples)
@@ -44,3 +13,38 @@ function unflatten_parameters(pflat,tuples)
     newp = vcat(newp,(pflat[tuples[end]],pflat[tuples[end]+1]))
     return vcat(newp,[pflat[j] for j in tuples[end]+2:length(pflat)])
 end
+
+function circuit_fit(circuit, ω_data,Z_data)
+
+    #getting initial parameters and the parameter shape
+    tuples = findall(x->typeof(x)!=Float64,get_params(circuit))
+    tuples += collect(0:length(tuples)-1)
+    unflatten = x->unflatten_parameters(x,tuples)
+
+    p = get_params(circuit)
+    p0 = collect(Iterators.flatten(p))
+    if p0 == p
+        unflatten = x->x
+    end
+    function fitting_function(circuit,ω,p)
+        p_shaped = unflatten(p)
+        Z = (set_params(circuit,p_shaped)~ω).Z
+        return vcat(real(Z),imag(Z))
+    end
+    fit_funct = (ω,p)->fitting_function(circuit,ω,p)
+    fit = curve_fit(fit_funct, ω_data, vcat(real(Z_data),imag(Z_data)), p0;
+        lower = zeros(length(p0)))
+    p_fit = unflatten(round.(fit.param,sigdigits = 5))
+    circuit_fit = set_params(circuit,p_fit)
+    println("Parameters")
+    println("__________")
+    print(circuit_fit.elements)
+    circuit_fit = circuit_fit ~ ω_data
+    Z_fit = circuit_fit.Z
+    plt = scatter(Z_data,label = "data")
+    scatter!(plt,Z_fit,markersize=3,label = "fit")
+    plot!(plt,aspect_ratio=:equal,yflip=true,ylabel = "-Im Z / Ω", xlabel = "Re Z / Ω",legend = :topleft)
+    display(plt)
+    return circuit_fit
+end
+
