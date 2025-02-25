3540b9099c1adb4807cf5b3088a5dfa66120369a
bdamerau <35343746+bdamerau@users.noreply.github.com>
2025-02-25 15:43:08 -0500

    Update circuit_fitting.jl

--- test/circuit_fitting.jl
+++ test/circuit_fitting.jl
@@ -4,7 +4,7 @@ using EISAnalysis: unflatten_parameters,get_params
 eval(initialize())
 
 @testset "circuit_fit" begin
-    randles_circuit = r-(r-0.025wo^80)/0.2c
+    randles_circuit = 0.23r-(r-0.025wo^80)/0.2c
     randles_circuit ~ [1.0,2.0,3.0]
     # p = get_params(randles_circuit)
     # @test p == [0.23,1.0,(0.025,80),0.2]
